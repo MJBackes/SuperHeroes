e16adbe8c34aa34ba587c5d9c2808277d624263d
MJBackes <mjohnbackes@gmail.com>
2019-11-21 15:38:44 -0600

    Imported a bootstrap theme from Bootswatch and added Hero images to the backgrounds.

--- SuperHeroes/App_Start/BundleConfig.cs
+++ SuperHeroes/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace SuperHeroes
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/home.css"));
         }
     }
 }
