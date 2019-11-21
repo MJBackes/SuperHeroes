4d6ed527110c6146a873e8b4001321ee9c91548e
MJBackes <mjohnbackes@gmail.com>
2019-11-21 10:57:56 -0600

    Initial Migration and created a HeroController controller.

---
+++ SuperHeroes/Controllers/HeroController.cs
@@ -0,0 +1,90 @@
+﻿using SuperHeroes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperHeroes.Controllers
+{
+    public class HeroController : Controller
+    {
+        // GET: Hero
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Hero/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Hero/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Hero/Create
+        [HttpPost]
+        public ActionResult Create(SuperHero hero)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Hero/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Hero/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, SuperHero hero)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Hero/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Hero/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, SuperHero hero)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- SuperHeroes/Models/IdentityModels.cs
+++ SuperHeroes/Models/IdentityModels.cs
@@ -20,6 +20,7 @@ namespace SuperHeroes.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<SuperHero> SuperHeroes { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
