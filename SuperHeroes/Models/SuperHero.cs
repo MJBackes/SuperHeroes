2427d9f155e128709240fba0f429caa2530cb9ae
MJBackes <mjohnbackes@gmail.com>
2019-11-21 10:38:10 -0600

    Added SuperHero class to Models folder.

---
+++ SuperHeroes/Models/SuperHero.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SuperHeroes.Models
+{
+    public class SuperHero
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AlterEgo { get; set; }
+        public string PrimaryAbility { get; set; }
+        public string Catchphrase { get; set; }
+    }
+}
